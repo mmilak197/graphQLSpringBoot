4d9fa0ad312d86f72b80d473d6ebea8408e10563
Milak <mzma@gft.com>
2020-06-16 14:25:27 +0200

    change java version to 11 and add first mutation

--- src/main/java/com/devmilak/graphQLProject/service/DeveloperService.java
+++ src/main/java/com/devmilak/graphQLProject/service/DeveloperService.java
@@ -1,5 +1,6 @@
 package com.devmilak.graphQLProject.service;
 
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import com.coxautodev.graphql.tools.GraphQLResolver;
 import com.devmilak.graphQLProject.model.Developer;
@@ -7,11 +8,12 @@ import com.devmilak.graphQLProject.repository.DeveloperRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-public class DeveloperService implements GraphQLQueryResolver {
+public class DeveloperService implements GraphQLQueryResolver, GraphQLMutationResolver {
 
     @Autowired
     private DeveloperRepository developerRepository;
@@ -24,5 +26,14 @@ public class DeveloperService implements GraphQLQueryResolver {
         return developerRepository.findAll().stream()
                                             .filter(it -> level.equals(it.getLevel()))
                                             .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public Developer updateLevel(final Long id, final String level) {
+         var developer = developerRepository.findById(id).get();
+         developer.setLevel(level);
+         developerRepository.save(developer);
+
+         return developer;
     }
 }
